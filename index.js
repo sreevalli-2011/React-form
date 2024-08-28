65c1db1f539522001de20007017b77f0fb1ec6ca
sreevalli <63503886+sreevalli-2011@users.noreply.github.com>
2024-08-28 08:21:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+import { getCLS } from 'web-vitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+ 
+
+  
+  getCLS(console.log);
+
+
